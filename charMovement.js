40f8bbad41171a9b92301e0dba7989085a086ea3
carvalhopaablo <150751693+carvalhopaablo@users.noreply.github.com>
2023-11-28 02:18:52 -0300

    Update charMovement.js

--- charMovement.js
+++ charMovement.js
@@ -1,43 +1,44 @@
-		const image = document.getElementById('char');
-        let positionX = 0;
-        let positionY = 0;
-        
-		let isFlipped = false;
-		
-		const zoomLevel = window.devicePixelRatio;
-		
-		const speed = zoomLevel*40;
+const image = document.getElementById('char');
+let positionX = 0;
+let positionY = 0;
+let isFlipped = false;
+
+const zoomLevel = window.devicePixelRatio;
+const speed = zoomLevel * 80;
 
 console.log("Nível de Zoom: " + zoomLevel);
 
-        document.addEventListener('keydown', (event) => {
-        
-		if (event.key === 'ArrowLeft') {
-            isFlipped = true;
-            image.style.transform = 'scaleX(-1)';
-        } else if (event.key === 'ArrowRight') {
-            isFlipped = false;
-            image.style.transform = 'scaleX(1)';
-        }
-		
-		switch (event.key) {
-                case 'ArrowUp':
-                    positionY -= speed;
-                    break;
-					
-                case 'ArrowDown':
-                    positionY += speed;
-                    break;
-					
-                case 'ArrowLeft':
-                    positionX -= speed;
-                    break;
-					
-                case 'ArrowRight':
-                    positionX += speed;
-                    break;
-            }
+// Adiciona ouvintes de eventos para os botões clicáveis
+document.getElementById('btnLeft').addEventListener('click', () => {
+    isFlipped = true;
+    image.style.transform = 'scaleX(-1)';
+    positionX -= speed;
+    updateImagePosition();
+	window.scrollBy(-speed, 0);
+});
 
-            image.style.left = positionX + 'px';
-            image.style.top = positionY + 'px';
-        });
+document.getElementById('btnRight').addEventListener('click', () => {
+    isFlipped = false;
+    image.style.transform = 'scaleX(1)';
+    positionX += speed;
+    updateImagePosition();
+	window.scrollBy(speed, 0);
+});
+
+document.getElementById('btnUp').addEventListener('click', () => {
+    positionY -= speed;
+    updateImagePosition();
+	window.scrollBy(0, -speed);
+});
+
+document.getElementById('btnDown').addEventListener('click', () => {
+    positionY += speed;
+    updateImagePosition();
+	window.scrollBy(0, speed);
+});
+
+// Função para atualizar a posição da imagem
+function updateImagePosition() {
+    image.style.left = positionX + 'px';
+    image.style.top = positionY + 'px';
+}
