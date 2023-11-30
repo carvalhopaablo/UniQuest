7c4d4a8d77ceccaebb12c03ba926f0b413c6d820
carvalhopaablo <150751693+carvalhopaablo@users.noreply.github.com>
2023-11-30 03:41:56 -0300

    Update charMovement.js

--- charMovement.js
+++ charMovement.js
@@ -4,35 +4,51 @@ let positionY = 0;
 let isFlipped = false;
 
 const zoomLevel = window.devicePixelRatio;
-const speed = 80;
+const speed = 32;
+
+window.addEventListener('load', () => {
+    // Redefine a posição da barra de rolagem para o topo e à esquerda
+    window.scrollTo(0, 0);
+});
 
 // Adiciona ouvintes de eventos para os botões clicáveis
 document.getElementById('btnLeft').addEventListener('click', () => {
-    isFlipped = true;
-    image.style.transform = 'scaleX(-1)';
-    positionX -= speed;
-    updateImagePosition();
-	window.scrollBy(-speed, 0);
+    if(positionX > 0){
+		window.scrollBy(-speed, 0);
+		isFlipped = true;
+		image.style.transform = 'scaleX(-1)';
+		positionX -= speed;
+		updateImagePosition();
+	}
+
 });
 
 document.getElementById('btnRight').addEventListener('click', () => {
-    isFlipped = false;
-    image.style.transform = 'scaleX(1)';
-    positionX += speed;
-    updateImagePosition();
-	window.scrollBy(speed, 0);
+	if(positionX < 1248){
+		isFlipped = false;
+		image.style.transform = 'scaleX(1)';
+		positionX += speed;
+		updateImagePosition();
+		window.scrollBy(speed, 0);
+		console.log(positionX);
+	}
 });
 
 document.getElementById('btnUp').addEventListener('click', () => {
-    positionY -= speed;
-    updateImagePosition();
-	window.scrollBy(0, -speed);
+    if(positionY > 0){
+		positionY -= speed;
+		updateImagePosition();
+		window.scrollBy(0, -speed);
+	}
 });
 
 document.getElementById('btnDown').addEventListener('click', () => {
-    positionY += speed;
-    updateImagePosition();
-	window.scrollBy(0, speed);
+	if(positionY < 1216){
+		positionY += speed;
+		updateImagePosition();
+		window.scrollBy(0, speed);
+		console.log(positionY);
+	}
 });
 
 // Função para atualizar a posição da imagem
